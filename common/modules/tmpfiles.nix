220a3524f94f76fab45cf1abaf663bc982019499
yuuhikaze <yuuhikaze@protonmail.com>
2025-12-02 16:39:29 -0500

    docs: adds clarification about journal CoW

--- common/modules/tmpfiles.nix
+++ common/modules/tmpfiles.nix
@@ -3,6 +3,7 @@
     rules = [
       # Journaling performs heavy writes
       # Disable Btrfs CoW to improve performance (no gazillion copies are made)
+      # A warning on first boot is expected since journal directory is created BEFORE tmpfiles runs
       "h /var/log/journal - - - - +C"
     ];
   };
